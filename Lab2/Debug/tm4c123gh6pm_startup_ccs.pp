9c47b1017cf7508eace4bb45f54568be053d1cfe
Jacob Williamson <jacobwilliamson@wireless-10-145-44-107.public.utexas.edu>
2016-02-10 16:12:45 -0600

    Lab3 Outline

--- Lab2/Debug/tm4c123gh6pm_startup_ccs.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c
-tm4c123gh6pm_startup_ccs.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
-
-../tm4c123gh6pm_startup_ccs.c: 
-/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
