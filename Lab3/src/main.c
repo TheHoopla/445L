c20368ae2bcfd2dc3a726701a235d41c6f305938
TheHoopla <jacobwilliamson@utexas.edu>
2016-02-15 21:39:36 -0600

    working commit

--- Lab3/src/main.c
+++ Lab3/src/main.c
@@ -21,6 +21,39 @@ uint32_t timeMinutes = 0;
 uint32_t alarmHours = 0;
 uint32_t alarmMinutes = 0;
 uint32_t alarmBool= 0;
+uint32_t secondsCount = 0;
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+
+void Timer0A_Init100HzInt(void){
+  volatile uint32_t delay;
+  DisableInterrupts();
+  // **** general initialization ****
+  SYSCTL_RCGCTIMER_R |= 0x01;      // activate timer0
+  delay = SYSCTL_RCGCTIMER_R;      // allow time to finish activating
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = 0;                // configure for 32-bit timer mode
+  // **** timer0A initialization ****
+                                   // configure for periodic mode
+  TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD;
+  TIMER0_TAILR_R = 79999999;         // 1 minute interrupts
+  TIMER0_IMR_R |= TIMER_IMR_TATOIM;// enable timeout (rollover) interrupt
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// clear timer0A timeout flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 32-b, periodic, interrupts
+  // **** interrupt initialization ****
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}
+
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;	// acknowledge timer0A timeout
+  if(secondsCount < 60){
+	  secondsCount++;
+  }
+
+}
 
 void setTime(void){
 	//update ST7735 for Set_Time_View
@@ -35,7 +68,7 @@ void setTime(void){
 			          //update ST7735
 					  break;
 			default:
-					  break;
+				      break;
 		}
 		buttonPressed = pullCurrentButton();
 	}
@@ -53,7 +86,7 @@ void setAlarm(void){
 					  //update ST7735
 					  break;
 			default:
-					  break;
+			          break;
 		}
 		buttonPressed = pullCurrentButton();
 	}
@@ -87,13 +120,14 @@ void screenManager(uint32_t selection){
 					alarmToggle();
 					break;
 			default:
-					break;
+				    break;
 	}
 	buttonPressed = pullCurrentButton();
 }
 
 int main(void) {
 	uint32_t selection = 0;
+	Timer0A_Init100HzInt();
 	while(1){
 		switch (buttonPressed){
 		//delay
@@ -113,7 +147,7 @@ int main(void) {
 				screenManager(selection);
 				break;
 		default:
-				break;
+			    break;
 		}
 		buttonPressed = pullCurrentButton();
 	}
