f8dc6cc56e8294838d98400388254403d237c7da
Jacob Williamson <jacobwilliamson@jacobs-air.attlocal.net>
2016-02-15 21:02:35 -0600

    removed all .DSStore

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/81/e0e8858259d400151495eb21a90706c9
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/PLL.c \
+../src/ST7735.c \
+../src/fixed.c \
+../src/main.c 
+
+C_DEPS += \
+./src/PLL.pp \
+./src/ST7735.pp \
+./src/fixed.pp \
+./src/main.pp 
+
+OBJS += \
+./src/PLL.obj \
+./src/ST7735.obj \
+./src/fixed.obj \
+./src/main.obj 
+
+OBJS__QUOTED += \
+"src/PLL.obj" \
+"src/ST7735.obj" \
+"src/fixed.obj" \
+"src/main.obj" 
+
+C_DEPS__QUOTED += \
+"src/PLL.pp" \
+"src/ST7735.pp" \
+"src/fixed.pp" \
+"src/main.pp" 
+
+C_SRCS__QUOTED += \
+"../src/PLL.c" \
+"../src/ST7735.c" \
+"../src/main.c" 
+
+
---
+++ Lab3/Debug/main.pp
@@ -0,0 +1,49 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/limits.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h
+main.obj: ../switch.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h
+main.obj: ../timer.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h
+main.obj: ../speaker.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h
+main.obj: ../../Lab1/src/ST7735.c
+main.obj: /Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h
+main.obj: ../../Lab1/src/ST7735.h
+main.obj: ../../Lab1/src/../../inc/tm4c123gh6pm.h
+
+../main.c: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/limits.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h: 
+../switch.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h: 
+../timer.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h: 
+../speaker.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdlib.h: 
+../../Lab1/src/ST7735.c: 
+/Applications/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdio.h: 
+../../Lab1/src/ST7735.h: 
+../../Lab1/src/../../inc/tm4c123gh6pm.h: 
