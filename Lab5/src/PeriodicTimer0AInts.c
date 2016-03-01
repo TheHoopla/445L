d1077e723e6a15d528e8976f635212b7c2043393
victordoval <victordoval@utexas.edu>
2016-03-01 02:02:14 -0600

    Lab5 Working Commit

--- Lab5/src/PeriodicTimer0AInts.c
+++ Lab5/src/PeriodicTimer0AInts.c
@@ -34,6 +34,7 @@
 #include <stdint.h>
 #include "PLL.h"
 #include "Timer0A.h"
+#include "switch.h"
 
 
 #define PF1       (*((volatile uint32_t *)0x40025008))
@@ -46,25 +47,102 @@
 #define WHEELSIZE 8           // must be an integer multiple of 2
                               //    red, yellow,    green, light blue, blue, purple,   white,          dark
 const long COLORWHEEL[WHEELSIZE] = {RED, RED+GREEN, GREEN, GREEN+BLUE, BLUE, BLUE+RED, RED+GREEN+BLUE, 0};
+int count = 0;
+int time = 0;
+const uint16_t Flute[32] = {1007,1252,1374,1548,1698,1797,1825,1797,1675,1562,1383,
+								 1219,1092,1007,913,890,833,847,810,777,744,674,
+								  598,551,509,476,495,533,589,659,758,876
+};
 
+const uint16_t wave[32] = {
+  2048*2,2448*2,2832*2,3186*2,3496*2,3751*2,3940*2,4057*2,4095*2,4057*2,3940*2,
+  3751*2,3496*2,3186*2,2832*2,2448*2,2048*2,1648*2,1264*2,910*2,600*2,345*2,
+  156*2,39*2,0*2,39*2,156*2,345*2,600*2,910*2,1264*2,1648*2
+};
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 long StartCritical (void);    // previous I bit, disable interrupts
 void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
+void DAC_Out(uint16_t code);
+
+void playMusic(){
+	//DAC_Out(256);
+	DAC_Out(Flute[count] * 2);
+	count = (count + 1) % 32;
+}
+
+//********DAC_Init*****************
+// Initialize TLV5616 12-bit DAC
+// inputs:  initial voltage output (0 to 4095)
+// outputs: none
+// assumes: system clock rate less than 20 MHz
+void DAC_Init(uint16_t data){
+  SYSCTL_RCGCSSI_R |= 0x01;       // activate SSI0
+  SYSCTL_RCGCGPIO_R |= 0x01;      // activate port A
+  while((SYSCTL_PRGPIO_R&0x01) == 0){};// ready?
+  GPIO_PORTA_AFSEL_R |= 0x2C;     // enable alt funct on PA2,3,5
+  GPIO_PORTA_DEN_R |= 0x2C;       // configure PA2,3,5 as SSI
+  GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R&0xFF0F00FF)+0x00202200;
+  GPIO_PORTA_AMSEL_R = 0;         // disable analog functionality on PA
+  SSI0_CR1_R = 0x00000000;        // disable SSI, master mode
+  SSI0_CPSR_R = 0x02;             // 8 MHz SSIClk
+  SSI0_CR0_R &= ~(0x0000FFF0);    // SCR = 0, SPH = 0, SPO = 0 Freescale
+  SSI0_CR0_R |= 0x0F;             // DSS = 16-bit data
+  SSI0_DR_R = data;               // load 'data' into transmit FIFO
+  SSI0_CR1_R |= 0x00000002;       // enable SSI
+
+}
+
+//********DAC_Out*****************
+// Send data to Max5353 12-bit DAC
+// inputs:  voltage output (0 to 4095)
+// outputs: none
+void DAC_Out(uint16_t code){
+  while((SSI0_SR_R&0x00000002)==0){};// SSI Transmit FIFO Not Full
+  SSI0_DR_R = code;                  // data out, no reply
+}
+
+#define C7 (50000000/2093)
+#define D7 (50000000/2349)
+#define E7 (50000000/2637)
+#define F7 (50000000/2793)
+#define G7 (50000000/3135)
+#define A7 (50000000/3520)
+#define B7 (50000000/3951)
+#define C8 (50000000/4186)
 
 void UserTask(void){
-  static int i = 0;
-  LEDS = COLORWHEEL[i&(WHEELSIZE-1)];
-  i = i + 1;
+//  static int i = 0;
+//  LEDS = COLORWHEEL[i&(WHEELSIZE-1)];
+//  i = i + 1;
+  playMusic();
+  time++;
+  if(time == 4000)
+  TIMER0_TAILR_R = D7 - 1;
+  if(time == 8000)
+  TIMER0_TAILR_R = E7 - 1;
+  if(time == 12000)
+  TIMER0_TAILR_R = F7 - 1;
+  if(time == 16000)
+  TIMER0_TAILR_R = G7 - 1;
+  if(time == 20000)
+  TIMER0_TAILR_R = A7 - 1;
+  if(time == 24000)
+  TIMER0_TAILR_R = B7 - 1;
+  if(time == 28000)
+  TIMER0_TAILR_R = C8 - 1;
 }
 // if desired interrupt frequency is f, Timer0A_Init parameter is busfrequency/f
 #define F16HZ (50000000/16)
+#define F10KHZ (50000000/10000)
 #define F20KHZ (50000000/20000)
 //debug code
 int main(void){ 
   PLL_Init(Bus80MHz);              // bus clock at 50 MHz
+  DAC_Init(1024);
+  Switch_Init();
   SYSCTL_RCGCGPIO_R |= 0x20;       // activate port F
   while((SYSCTL_PRGPIO_R&0x0020) == 0){};// ready?
   GPIO_PORTF_DIR_R |= 0x0E;        // make PF3-1 output (PF3-1 built-in LEDs)
@@ -75,7 +153,7 @@ int main(void){
   GPIO_PORTF_AMSEL_R = 0;          // disable analog functionality on PF
   LEDS = 0;                        // turn all LEDs off
 //  Timer0A_Init(&UserTask, F20KHZ);     // initialize timer0A (20,000 Hz)
-  Timer0A_Init(&UserTask, F16HZ);  // initialize timer0A (16 Hz)
+  Timer0A_Init(&UserTask, C7);  // initialize timer0A (16 Hz)
   EnableInterrupts();
 
   while(1){
---
+++ Lab5/src/switch.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "PLL.h"
+#include "../../inc/tm4c123gh6pm.h"
+
+int pullCurrentButton(void);
+void Switch_Init(void);
+unsigned long Switch_Input(void);
+
